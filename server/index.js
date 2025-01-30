a31476070589a7cde3e9e3b925d85eb1450f313c
Bruno Mattos <brunoandremattos@gmail.com>
2025-01-30 16:05:53 -0300

    began adding auth

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
+import authRoutes from "./routes/auth.js"
+import { register } from "./controllers/auth.js"
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url)
@@ -37,6 +39,12 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({ storage })
+
+// Routes with files
+app.post("/auth/resgister", upload.single("picture"), register)
+
+// Routes
+app.use("/auth", authRoutes)
 
 // Mongoose setup
 const PORT = process.env.PORT || 6001
