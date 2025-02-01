2b2b9da23479d2be3a03246904a34525bff19dfa
Bruno Mattos <brunoandremattos@gmail.com>
2025-01-31 22:34:01 -0300

    added posts logic

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
 import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
 import { register } from "./controllers/auth.js"
+import { createPost } from "./controllers/posts.js"
+import { verifyToken } from "./middleware/auth.js"
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url)
@@ -41,10 +45,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 // Routes with files
-app.post("/auth/resgister", upload.single("picture"), register)
+app.post("/auth/register", upload.single("picture"), register)
+app.post("/posts", verifyToken, upload.single("picture"), createPost)
 
 // Routes
 app.use("/auth", authRoutes)
+app.use("/users", userRoutes)
+app.use("/posts", postRoutes)
 
 // Mongoose setup
 const PORT = process.env.PORT || 6001
